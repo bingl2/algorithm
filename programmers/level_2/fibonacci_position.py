a4750682e5f34f6eb7d49951060261b7d165d6e0
Hyunbin.Choi <chlgusrla@gmail.com>
2018-07-21 11:54:57 +0900

    해결법 시간 비교. 확실하게 숫자가 커질 수록 두번째 방법이 더 효율적이다.

--- programmers/level_2/fibonacci_position.py
+++ programmers/level_2/fibonacci_position.py
@@ -15,6 +15,8 @@ n	   return
 피보나치수는 0번째부터 0, 1, 1, 2, 3, 5, ... 와 같이 이어집니다.
 """
 
+from util.time_util import runtime
+
 
 # 일단 피보나치 수를 보면 [(숫자)는 자리를 의미함].
 # 0(0) + [1(1) = 1(2)]
@@ -22,6 +24,7 @@ n	   return
 # 1(2) + [2(3) = 3(4)]
 # 2(3) + [3(4) = 5(5)]
 # 간단하게 대괄호에 쌓인 숫자들이, 다음 수를 구할 때 사용되는 공통점을 발견할 수 있다.
+@runtime
 def first_solution(position):
     # 시작할 피노차리 수열 리스트를 만든다.
     fibo_list = [0, 1]
@@ -35,12 +38,14 @@ def first_solution(position):
     return fibo_list[position] % 1234567
 
 
-assert first_solution(3) == 2
-assert first_solution(5) == 5
-assert first_solution(1000) == 138544
+assert first_solution(5000) == 1178031
+assert first_solution(10000) == 1107030
+assert first_solution(100000) == 1168141
+assert first_solution(200000) == 672799
 
 
 # 원하는 포지션 값에서 "1234567"을 나눈 나머지를 구해주면 되니 리스트를 빼고 작성.
+@runtime
 def second_solution(position):
     first, second = 0, 1
 
@@ -59,6 +64,7 @@ def second_solution(position):
     return first % 1234567
 
 
-assert second_solution(3) == 2
-assert second_solution(5) == 5
-assert second_solution(1000) == 138544
+assert second_solution(5000) == 1178031
+assert second_solution(10000) == 1107030
+assert second_solution(100000) == 1168141
+assert second_solution(200000) == 672799
