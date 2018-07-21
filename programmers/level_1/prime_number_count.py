7000dbd2f9808da516d201e5cdd958834074f1f5
Hyunbin.Choi <chlgusrla@gmail.com>
2018-07-21 11:54:23 +0900

    2번째 해결법

--- programmers/level_1/prime_number_count.py
+++ programmers/level_1/prime_number_count.py
@@ -21,18 +21,7 @@ n	result
 """
 
 
-def runtime(func):
-    import time
-
-    def print_time(*args):
-        start_time = time.time()
-        result = func(*args)
-
-        print(func.__name__ + " (1 ~ " + "{:,}".format(*args) + ")")
-        print(time.time() - start_time)
-
-        return result
-    return print_time
+from util.time_util import runtime
 
 
 @runtime
