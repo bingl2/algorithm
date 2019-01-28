644c8d433729ebdcde298434823f0ddeebec94ce
Hyunbin.Choi <chlgusrla@gmail.com>
2019-01-28 23:08:57 +0900

    풀이 개선

--- programmers/level_2/phone_number_list.py
+++ programmers/level_2/phone_number_list.py
@@ -17,23 +17,13 @@ phone_book의 길이는 1 이상 1,000,000 이하입니다.
 
 
 def solution(phone_book):
-    answer = True
+    phone_book = sorted(phone_book)
 
-    for pn_index, phone_num in enumerate(phone_book):
-        for cpn_index, check_phone_num in enumerate(phone_book):
-            if pn_index == cpn_index:
-                continue
+    for pn, pn2 in zip(phone_book, phone_book[1:]):
+        if pn2.startswith(pn):
+            return False
 
-            if len(phone_num) > len(check_phone_num):
-                break
-
-            check_phone_num = check_phone_num[:len(phone_num)]
-
-            if phone_num == check_phone_num:
-                answer = False
-                break
-
-    return answer
+    return True
 
 
 assert solution(["119", "97674223", "1195524421"]) == False
@@ -46,6 +36,15 @@ assert solution(["567", "1235", "12", "88", "123"]) == False
 정말 무식하게 빅오 n의 2승으로 풀이.
 자기 자신은 패스, 접두어 이기 때문에 만약 검사해야할 길이보다 길 경우 중단.
 배열을 이용하여 길이만큼 체크하고, 같을 경우 False 처리 진행.
- 
+
+빠른 풀이후 개선 방법
+
+- 속도 개선
+1. 배열을 길이별로 정리를 할 수 있다면 비교해할 부분이 상당수 줄어들것 같다.  
+
+
+- 읽기 쉬운 코드 개선
+1. 필요한 로직 파악이 필요, 숫자가 들어있는 배열을 정렬 하면 앞자리의 숫자 크기 순서데로 배열이됨.
+2. 만약 '뒤의 수' 가, '앞의 수' 를 포함하지 않을 경우 '뒤의 수의 뒤의 수' 는 당연히 '앞의 수' 를 포함하지 않음.
 
 """
